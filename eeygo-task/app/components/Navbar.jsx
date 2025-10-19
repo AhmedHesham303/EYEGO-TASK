030a8035f824865c1c4c9db867abae18c8c6fe06
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-10-19 18:19:57 +0300

    responsivnise finshed for all pages

--- eeygo-task/app/components/Navbar.jsx
+++ eeygo-task/app/components/Navbar.jsx
@@ -4,6 +4,10 @@ import Image from "next/image";
 import { useDispatch } from "react-redux";
 import { useRouter } from "next/navigation";
 export default function Navbar() {
+  const items = [
+    { name: "Dashboard", href: "/dashboard" },
+    { name: "Users", href: "/dashboard/users" },
+  ];
   const dispatch = useDispatch();
   const router = useRouter();
   const handelLogoutEvent = () => {
@@ -14,6 +18,18 @@ export default function Navbar() {
   return (
     <nav className="flex justify-between items-center bg-white shadow px-8 ">
       <Image src="/eyegoai_logo.jpeg" alt="Logo" width={60} height={20} />
+      <ul className=" flex  gap-4 text-sm md:hidden">
+        {items.map((item) => (
+          <li key={item.href}>
+            <Link
+              href={item.href}
+              className="hover:text-blue-600 font-medium transition-colors"
+            >
+              {item.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
       <Link href="/" className="text-blue-600 hover:underline">
         <button onClick={handelLogoutEvent} className="hover:cursor-pointer">
           Logout
--- eeygo-task/app/page.js
+++ eeygo-task/app/page.js
@@ -37,24 +37,26 @@ export default function LoginPage() {
         className="mb-4"
       />
 
-      <div className="bg-white p-8 md:p-10 rounded-2xl shadow-lg border border-slate-100 w-1/4 mx-auto">
+      <div className="bg-white p-8 md:p-10 rounded-2xl shadow-lg border border-slate-100 w-/1/2  md:w-1/3 lg:w-1/4 mx-auto">
         <form
           onSubmit={handelLoginEvent}
           className="flex items-center justify-center text-black flex-col gap-4 w-full"
         >
           <div className="w-full">
-            <label className="text-sm font-medium text-slate-700">Email</label>
+            <label className="text-xs md:text-md  font-light md:font-medium text-slate-700">
+              Email
+            </label>
             <input
               type="email"
               required
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="w-full px-3 py-3 border border-slate-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              className="w-full px-1 py-1 md:px-3 md-py-3  border border-slate-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 text-sm md:text-sm"
             />
           </div>
 
           <div className="w-full">
-            <label className="text-sm font-medium text-slate-700">
+            <label className="text-xs md:text-md  font-light md:font-medium text-slate-700">
               Password
             </label>
             <input
@@ -62,16 +64,17 @@ export default function LoginPage() {
               required
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="w-full px-3 py-3 border border-slate-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+              className="w-full px-1 py-1 md:px-3 md-py-3  border border-slate-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 text-sm md:text-sm"
             />
           </div>
 
           <button
-            className="h-9 px-8 py-6 flex items-center justify-center text-lg rounded-lg border border-transparent text-white bg-blue-600 hover:bg-blue-700 hover:cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-all duration-200 active:scale-95"
+            className="h-6 px-4 py-3 text-xs md:text-md md:h-9 md:px-8 md:py-4 md:text-lg flex items-center justify-center rounded-lg border border-transparent text-white bg-blue-600 hover:bg-blue-700 hover:cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-all duration-200 active:scale-95"
             type="submit"
           >
             {loading ? "Loading..." : "Login"}
           </button>
+
           {error && (
             <div className="text-red-600" role="alert">
               {error}
